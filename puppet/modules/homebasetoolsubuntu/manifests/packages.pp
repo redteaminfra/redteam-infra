7d2e1ba768ebf38458b6d1584ecedced0ab64d0b
Michael Leibowitz <michael.l.leibowitz@oracle.com>
2019-10-18 10:32:06 -0700

    Add proxychains to homebase ubuntu
    proxychains is included in the kali toolset, but not the ubuntu one

--- puppet/modules/homebasetoolsubuntu/manifests/packages.pp
+++ puppet/modules/homebasetoolsubuntu/manifests/packages.pp
@@ -1,4 +1,4 @@
 class homebasetoolsubuntu::packages {
-    $packages = ['asciinema', 'xfce4', 'xfce4-goodies', 'tightvncserver', 'screen', 'tmux', 'zile', 'emacs', 'firefox', 'nmap', 'nikto']
+    $packages = ['asciinema', 'xfce4', 'xfce4-goodies', 'tightvncserver', 'screen', 'tmux', 'zile', 'emacs', 'firefox', 'nmap', 'nikto', 'proxychains']
     package { $packages: ensure => "installed" }
 }
