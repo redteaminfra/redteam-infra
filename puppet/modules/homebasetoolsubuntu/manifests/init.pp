1096b80c9089c3f8edf4c2186ff1e3d76d846ea2
Michael Leibowitz <michael.l.leibowitz@oracle.com>
2019-12-18 00:04:02 +0000

    Add a .xscreensaver to disable the xfce4 screensaver
    When the screensaver activates, it uses a considerable amount of
    CPU. Since it is only on VNC, the screensaver is not useful

--- puppet/modules/homebasetoolsubuntu/manifests/init.pp
+++ puppet/modules/homebasetoolsubuntu/manifests/init.pp
@@ -8,4 +8,12 @@ class homebasetoolsubuntu {
       ensure => present,
       source => "puppet:///modules/homebasetoolsubuntu/setup-xfce4-vnc"
     }
+
+    file { "/usr/local/lib/dot_screensaver":
+      path => '/usr/local/lib/dot_screensaver',
+      owner => 'root',
+      mode => '755',
+      ensure => present,
+      source => "puppet:///modules/homebasetoolsubuntu/dot_xscreensaver"
+    }
 }
