228c21db275583a8acff210824b0ed282c93d05b
Michael Leibowitz <michael.l.leibowitz@oracle.com>
2020-04-21 15:22:19 -0700

    homebasetoolsubuntu: whitespace

--- puppet/modules/homebasetoolsubuntu/manifests/packages.pp
+++ puppet/modules/homebasetoolsubuntu/manifests/packages.pp
@@ -1,4 +1,15 @@
 class homebasetoolsubuntu::packages {
-    $packages = ['asciinema', 'xfce4', 'xfce4-goodies', 'tightvncserver', 'screen', 'tmux', 'zile', 'emacs', 'firefox', 'nmap', 'nikto', 'proxychains4']
+    $packages = ['asciinema',
+                 'xfce4',
+                 'xfce4-goodies',
+                 'tightvncserver',
+                 'screen',
+                 'tmux',
+                 'zile',
+                 'emacs',
+                 'firefox',
+                 'nmap',
+                 'nikto',
+                 'proxychains4']
     package { $packages: ensure => "installed" }
 }
