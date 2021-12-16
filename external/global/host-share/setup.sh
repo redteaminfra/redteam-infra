cd8226abeb21e28b282dafef1d7f9131984dc3f0
Topher Timzen <topher.timzen@oracle.com>
2021-12-16 15:24:36 -0800

    Fix tee in setup.sh

--- external/global/host-share/setup.sh
+++ external/global/host-share/setup.sh
@@ -21,7 +21,7 @@ apt -y -qq install openjdk-8-jre-headless
 ### Setup GPG Key for logstash
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 apt-get install apt-transport-https
-echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" > /etc/apt/sources.list.d/elastic-6.x.list
 apt-get update
 
 ### Install puppet
