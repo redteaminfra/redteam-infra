c5b4f40141889cefe147a5843c058d0b542ae33c
Will Kinderman <will.kinderman@oracle.com>
2023-09-18 16:58:25 -0700

    Ensure just TF state is used
    Fixes RTI-203.
    Don't need to filter on labels, just pull IP from TF state

--- external/sketch/linode/output.tf
+++ external/sketch/linode/output.tf
@@ -1,39 +1,3 @@
-data "linode_instances" "all-compute" {
-  depends_on = [linode_instance.middle, linode_instance.edge]
-  filter {
-    name = "tags"
-    values = [var.engagement_name]
-  }
-}
-
-data "linode_instances" "edge" {
-  depends_on = [linode_instance.middle, linode_instance.edge]
-  filter {
-    name   = "tags"
-    values = [var.engagement_name]
-  }
-
-    filter {
-    name     = "label"
-    values   = ["edge"]
-    match_by = "substring"
-  }
-}
-
-data "linode_instances" "middle" {
-  depends_on = [linode_instance.middle, linode_instance.edge]
-  filter {
-    name   = "tags"
-    values = [var.engagement_name]
-  }
-
-  filter {
-    name     = "label"
-    values   = ["middle"]
-    match_by = "substring"
-  }
-}
-
 resource "local_file" "ssh_stanza" {
   depends_on = [linode_instance.middle, linode_instance.edge]
   filename = pathexpand("${var.ssh_config_path}/${var.engagement_name}-sketch")
@@ -41,8 +5,8 @@ resource "local_file" "ssh_stanza" {
   content = templatefile("templates/ssh-stanza.tftpl", {
     engagement_name = var.engagement_name,
     ssh_private_key = var.ssh_private_key
-    middles = data.linode_instances.middle.instances.*,
-    edges = data.linode_instances.edge.instances.*
+    middles = linode_instance.middle.*,
+    edges = linode_instance.edge.*
   })
 }
 
@@ -51,8 +15,8 @@ resource "local_file" "ansible_inventory" {
   filename = "inventory.ini"
   file_permission = "0600"
   content = templatefile("templates/inventory.tftpl", {
-    middles = data.linode_instances.middle.instances.*,
-    edges = data.linode_instances.edge.instances.*
+    middles = linode_instance.middle.*,
+    edges = linode_instance.edge.*
   })
 }
 
