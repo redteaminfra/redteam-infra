8272643ee7667dfbb7afbef44e71ca7813838dd7
Will Kinderman <will.kinderman@oracle.com>
2023-10-27 12:57:40 -0700

    Notify user that work is being done on first login

--- ansible/roles/homebase/files/skel-append-profile.sh
+++ ansible/roles/homebase/files/skel-append-profile.sh
@@ -1,4 +1,5 @@
 if [[ ! -f "$HOME/.vnc_configured" ]]; then
+  echo "Configuring VNC server..."
   vncpasswd -f <<< $(whoami) > ~/.vnc/passwd
   chmod 600 ~/.vnc/passwd
 
@@ -9,4 +10,6 @@ if [[ ! -f "$HOME/.vnc_configured" ]]; then
   systemctl --user start vncserver@$PORT.service > /dev/null 2>&1
 
   touch "$HOME/.vnc_configured"
+  echo "Finished configuring VNC server."
+  echo "Have a nice day!"
 fi
