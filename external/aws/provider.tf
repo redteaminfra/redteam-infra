0a27d8b83c519b0ead6a391cd9cd76c0512df333
Will Kinderman <wkinderm@starbucks.com>
2024-02-14 09:39:39 -0800

    change auth mechanism
    Users were required to auth with secrets stored on disk. Change this so you just provide a profile for the aws account you would like to log in with. This gives the user the ability to use other login methods like `aws sso login`

--- external/aws/provider.tf
+++ external/aws/provider.tf
@@ -7,9 +7,7 @@ terraform {
 }
 
 provider "aws" {
-  shared_config_files = [ "~/.aws/config" ]
-  shared_credentials_files = [ var.shared_credentials_file ]
-  profile = "default"
+  profile = var.profile
 }
 
 provider "local" {}
