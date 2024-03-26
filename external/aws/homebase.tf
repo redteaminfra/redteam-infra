c6cf12d74c225b4d818cc63efaf7f2c12e4d1066
Will Kinderman <github.com@defective.computer>
2024-03-26 14:21:56 -0700

    Lifecycle Management for AMI and formatting
    Enable lifecycle management on EC2 ami. Ensure that EC2 isn't recreated if ami is changed.
    
    Minor reformatting of files touched, including making it easier for default_tags to be read

--- external/aws/homebase.tf
+++ external/aws/homebase.tf
@@ -1,20 +1,33 @@
 resource "aws_instance" "homebase" {
-  ami = data.aws_ami.ubuntu.id
-  instance_type = var.homebase_shape
-  key_name = aws_key_pair.deployer.key_name
-  subnet_id = aws_subnet.infra.id
-  private_ip = "192.168.0.10"
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.homebase_shape
+  key_name                    = aws_key_pair.deployer.key_name
+  subnet_id                   = aws_subnet.infra.id
+  private_ip                  = "192.168.0.10"
   associate_public_ip_address = true
+
   vpc_security_group_ids = [
     aws_security_group.ssh_from_company.id,
     aws_security_group.internal.id,
   ]
-   root_block_device {
+
+  metadata_options {
+    http_tokens = "required" # enable imds v2
+  }
+
+  root_block_device {
     volume_type = "standard"
     volume_size = var.boot_volume_size_in_gbs
   }
+
   tags = {
-    Op = var.engagement_name
+    Op   = var.engagement_name
     Name = format("homebase-%s", var.engagement_name)
+  }
+
+  lifecycle {
+    ignore_changes = [
+      ami,  # Ignore changes to the AMI, you don't want TF to destroy resources if it is updated
+    ]
   }
 }
